a6a1f6400ebe9208e961eb7c605044962707438a
Wafi Chaar <wafi.chaar@hloul-bas.com>
2019-03-28 20:41:59 +0200

    submodules

---
+++ crm_provetto/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'CRM Provetto',
+    'version': '1.5',
+    'category': 'Sales',
+    'sequence': 6,
+    'author': 'Media Engagers',
+    'summary': 'Crm Provetto',
+    'description': """
+
+=======================
+Provetto CRM
+
+""",
+    'depends': ['crm'],
+    'data': [
+        #'security/ir.model.access.csv',
+        'views/views.xml',
+        #'views/templates.xml'
+    ],
+    'installable': True,
+    'website': '',
+    'auto_install': False,
+}
---
+++ product_barcode/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import product_form
+from . import account_payment
+from . import stock_inventory
