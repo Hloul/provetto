b9cf61581d0e8818faa6c6591992e8dc1feaff44
Wafi Chaar <wafi.chaar@hloul-bas.com>
2019-05-25 23:28:45 +0300

    bug 02

--- activity_on_payment/models/account_payment.py
+++ activity_on_payment/models/account_payment.py
@@ -13,7 +13,7 @@ from odoo import models, fields
 
 class AccountPayment(models.Model):
     _inherit = ['account.payment','mail.activity.mixin']
-
+_name = 'account.payment'
 activity_ids = fields.One2many('mail.activity', string='Activities')
 
 
