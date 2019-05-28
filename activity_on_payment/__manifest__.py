0580f7d946a2d69fd24bb01319ba241f4b9b50d1
Wafi Chaar <wafi.chaar@hloul-bas.com>
2019-05-28 13:55:43 +0300

    account_payment final version

---
+++ activity_on_payment/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Activity on Payment',
+    'version': '12.0.1.0',
+    'category': 'Accounting',
+    'sequence': 1,
+    'author': 'HLOUL-BAS',
+    'summary': 'Add activity scheduling to payments',
+    'description': """
+HLOUL-BAS
+=======================
+Add the functionality of schedule an activity to payments model
+
+""",
+    'depends': ['mail','account'],
+    'data': ['views/views.xml'],
+    'installable': True,
+    'application': False,
+    'website': 'www.hloul-bas.com',
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ activity_on_payment/models/account_payment.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    ODOO, Open Source Management Solution
+#    Copyright (C) 2019 HLOUL-BAS (<http://www.hloul-bas.com>).
+#
+#    For Module Support : r2d2@hloul-bas.com 
+#
+##############################################################################
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class AccountPayment(models.Model):
+	_inherit = ['account.payment', 'mail.activity.mixin']
+	_name = 'account.payment'
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
